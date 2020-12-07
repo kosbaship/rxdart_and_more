5a7b85ec8d85e51bd1486c47c3af7cdd5ff30602
kosba <mohamed.kosba96@gmail.com>
2020-12-07 20:24:46 +0200

    Add Model Class (PosT)

---
+++ lib/models/post_model.dart
@@ -0,0 +1,9 @@
+class PostModel {
+  int id;
+  String title;
+  String author;
+  String content;
+  DateTime publishData;
+
+  PostModel({this.id, this.title, this.author, this.content, this.publishData});
+}
